2eb60f60fef79e0717981f2bc9b44ed75078ce68
Collado69 <audrey.sauvageon@yahoo.fr>
2019-05-14 10:50:50 +0200

    initial commit

---
+++ src/Form/PropertySearchType.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\PropertySearch;
+use App\Entity\Tag;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class PropertySearchType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('maxPrice', IntegerType::class,[
+                'required' => false,
+                'label' => false,
+                'attr' => [
+                    'placeholder' =>'Budget maximum'
+                ]
+            ])
+            ->add('minSurface', IntegerType::class,[
+                'required' => false,
+                'label' => false,
+                'attr' => [
+                    'placeholder' => 'Surface minimum'
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => PropertySearch::class,
+            'method' => 'get',
+            'csrf_protection' => false
+        ]);
+    }
+    
+    public function getBlockPrefix()
+    {
+        return '';
+    }
+}
